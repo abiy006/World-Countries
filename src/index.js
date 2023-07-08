e3a2dc6cd528df32d7ea0be7cc7d35c1a02daf0e
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-07-08 11:58:53 +0300

    Linter error on Missing file extension is resolved

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/store';
 import App from './App';
+import store from './redux/store';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
